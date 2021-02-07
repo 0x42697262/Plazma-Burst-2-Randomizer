4ac829234613a9f83f282470bead8b4b5b20adc3
KrulYuno <giogulane.wineffgulane@gmail.com>
2021-02-07 19:52:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ app = Flask(__name__)
 def index():
   return redirect("https://github.com/KrulYuno/Plazma-Burst-2-Randomizer", code=200)
 
-@app.route('/player')
+@app.route('/player', methods = ['POST'])
 def player():
     if "players" in request.args:
         return random.choice(request.args.get("players").split(','))
 
-@app.route('/randomize')
+@app.route('/randomize', methods = ['POST'])
 def randomize():
     return str(random.randrange(int(request.args.get("start", default="0")), int(request.args.get("stop", default="256"))))
    
